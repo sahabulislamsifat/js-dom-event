55bc5ee968a44b6e0366f820ea2b83a179a25489
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-18 00:12:24 +0600

    second commit

--- JS/event.js
+++ JS/event.js
@@ -1,1 +1,20 @@
-console.log("This is a Separate file of JS");
+// Option - 1 : Directly set on the HTML Element
+    // <button onclick="console.log(80)">Another Click</button>;
+
+// Option - 2 : add onclick function
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// Option - 3 :
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+// Another Option - 3 :
+const makePurpleButton = document.getElementById("make-purple");
+makePurpleButton.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple";
+};
